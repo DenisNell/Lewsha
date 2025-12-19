49f494c99e49e4f997f87f4491b3cfb3dd87c414
DenisNell <udindenis@yandex.ru>
2025-12-20 00:46:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -375,6 +375,16 @@ function activateGame() {
   const formButton = document.querySelector('.form_button')
   formButton.innerText = `Подсказка`
 
+// Пример с JavaScript
+const button = document.getElementById('myButton');
+
+button.addEventListener('click', function() {
+    button.disabled = true;
+    setTimeout(() => {
+        button.disabled = false;
+    }, 2000);
+});
+
   const nameInput = document.getElementById('name');
 
   rigthCounter.innerText = `${rigth}`
